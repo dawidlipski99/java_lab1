6dd9bdd1268af1af6cf7e4bc7203619bf8721bcd
Dawid Lipski <dawid.lipski99@wp.pl>
2021-12-18 20:30:05 +0100

    ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.5 */
+        /* Tablice - cw_6 */
 
-        /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
-         * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
-         * */
-        int wiek = 22 % 3;
-        System.out.println(wiek); // 2
-        if (wiek == 0) {
-            System.out.println("podzielny przez 3");
-        } else {
-            System.out.println("niepodzielny przez 3");
+        /* zad1. stworzyć tablicę typu double 5-cio elementową,
+        przypisać do niej wartości i wyświetlić */
+
+        double[] tablica = new double[5];
+        tablica[0] = 20;
+        tablica[1] = 5;
+        tablica[2] = 12.65;
+        tablica[3] = 15.60;
+        tablica[4] = 18.18;
+        for(int i=0; i < tablica.length; i++) {
+            System.out.println(tablica[i]);
         }
-
-
-
-
-        /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
-
-        int numer_albumu = 57081;
-        int parzystosc = (numer_albumu % 2 == 0) ? 0 : 1;
-        System.out.println("Parzystość: " +parzystosc);
-
-        /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
-         * i stwórz konstrukcję else if z dowolnymi komunikatami */
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Podaj losową liczbę");
-        double first = scanner.nextDouble();
-        System.out.println("Podaj drugą losową liczbę");
-        double second = scanner.nextDouble();
-        double suma = first + second;
-
-        System.out.println("Wynik dodania tych liczb to" +suma);
-
     }
 }
