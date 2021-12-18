7b9d6da31e55b856faa64f2a8ea0adc338940eb7
Dawid Lipski <dawid.lipski99@wp.pl>
2021-12-18 19:44:54 +0100

    ćwiczenie 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* ćw.1 */
+        System.out.println("Witam");
+        System.out.println("Pana lub Panią.");
+        System.out.printf("Jest %s mój %d program.\n", "to", 1 );
+
+        System.out.print("Mam nadzieję, że wszystko działa");
+        System.out.print(" pozdrawiam");
+        System.out.print(" serdecznie.");
+
+    }
+}
